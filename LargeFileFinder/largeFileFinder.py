2e7a5ca7008c3bcafd6f9139a3fa3721a2235e43
Landon <landonyoung71@gmail.com>
2024-01-02 16:52:33 -0300

    Added exception handling for missing files

--- LargeFileFinder/largeFileFinder.py
+++ LargeFileFinder/largeFileFinder.py
@@ -1,12 +1,13 @@
 import os
 
+
 def large_file_finder():
     path = input('Please enter a path')
     for root, dirs, files in os.walk(path):
         for file in files:
             my_path = os.path.join(root, file)
             if not os.path.exists(my_path):
-                continue
+                raise Exception(f'{my_path} does not exist or can\'t be located.')
             if os.path.getsize(my_path) > (100*1024*1024):  # files larger than 100mb
                 print(f'''
                         File name: {file}
@@ -14,4 +15,8 @@ def large_file_finder():
                         File path: {my_path}
                     ''')
 
-large_file_finder()
+
+try:
+    large_file_finder()
+except Exception as err:
+    print(str(err))
